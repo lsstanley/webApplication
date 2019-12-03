9622228508058e05cc007b58123366a0015b4506
jack morrison <55364225+jackdmorrison@users.noreply.github.com>
2019-12-03 18:14:24 +0000

    working javascript that shows the methods and ingredients as lists and switches to each recipe also changed some bits in the json

--- recipe.js
+++ recipe.js
@@ -21,15 +21,15 @@ fetch("https://raw.githubusercontent.com/lsstanley/webApplication/jacks_dev/reci
     recipeName[counter]=`${recipe.recipename}`;
     recipeDescription[counter]=`${recipe.desc}`;
     recipePic[counter]=`${recipe.picture}`;
-    recipeIngredients[counter]=`${recipe.Ingredients}`;
-    recipeMethod[counter]=`${recipe.Method}`;
-    //for (var i = 0; i < recipeIngredients[0].length ; i++) {
+    recipeIngredients[counter]=`${recipe.Ingredients}`.split(",");
+    recipeMethod[counter]=`${recipe.method}`.split(",");
+    for (var i = 0; i < recipeIngredients[0].length ; i++) {
     if(counter==0){
       let Li=document.createElement('li');
-      Li.innerHTML=recipeIngredients[counter];
+      Li.innerHTML=recipeIngredients[counter][i];
       ul.appendChild(Li);
     }
-    //}
+    }
     counter++;
     T.innerHTML=recipeName[0];
     D.innerHTML=recipeDescription[0];
@@ -42,50 +42,57 @@ fetch("https://raw.githubusercontent.com/lsstanley/webApplication/jacks_dev/reci
 });
 
 function changeNextRecipe(){
+  ul.innerHTML = "";
   if (counter<recipeName.length){
     counter++
   }
   else{
     counter=0;
   }
-  //for (var i = 0; i < recipeIngredients[counter].length; i++) {
+  for (var i = 0; i < recipeIngredients[counter].length; i++) {
     let Li=document.createElement('li');
-    Li.innerHTML=recipeIngredients[counter];
+    Li.innerHTML=recipeIngredients[counter][i];
     ul.appendChild(Li);
-  //}
+  }
 
   T.innerHTML=recipeName[counter];
-
   title.appendChild(T);
 
 }
 function changeLastRecipe(){
+  ul.innerHTML = "";
   if (counter!==0){
     counter--;
   }
   else{
     counter=recipeName.length;
   }
-  //for (var i = 0; i < recipeIngredients[counter].length; i++) {
+  for (var i = 0; i < recipeIngredients[counter].length; i++) {
     let Li=document.createElement('li');
-    Li.innerHTML=recipeIngredients[counter];
+    Li.innerHTML=recipeIngredients[counter][i];
     ul.appendChild(Li);
-  //}
+  }
   T.innerHTML=recipeName[counter];
   title.appendChild(T);
 
 }
 function changeMethod(){
-  //for (var i = 0; i < recipeMethod[0].length ; i++) {
+  ul.innerHTML = "";
+  for (var i = 0; i < recipeMethod[0].length ; i++) {
     let Li=document.createElement('li');
-    Li.innerHTML=recipeMethod[counter];
+    Li.innerHTML=recipeMethod[counter][i];
     ul.appendChild(Li);
-  //}
+  }
+  T.innerHTML=recipeName[counter];
+  title.appendChild(T);
 }
 function changeIngredients(){
-  //for (var i = 0; i < recipeIngredients[0].length ; i++) {
+  ul.innerHTML = "";
+  for (var i = 0; i < recipeIngredients[0].length ; i++) {
     let Li=document.createElement('li');
-    Li.innerHTML=recipeIngredients[counter];
+    Li.innerHTML=recipeIngredients[counter][i];
     ul.appendChild(Li);
-  //}
+  }
+  T.innerHTML=recipeName[counter];
+  title.appendChild(T);
 }
